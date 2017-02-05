836a730043f0bc084d53273cf14364d5af20bfbc
bergun <barisergun75@gmail.com>
2017-02-05 14:40:01 +0000

    assignment3: initial commit

---
+++ assignment3/app.js
@@ -0,0 +1,100 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowItDownApp', []).
+  controller('NarrowItDownController',NarrowItDownController).
+  service('MenuSearchService', MenuSearchService).
+  filter('searchTerm', SearchTermFilter).
+  directive('foundItems', FoundItemsDirective).
+  constant('MenuApiBasePath', "http://davids-restaurant.herokuapp.com").
+  constant('MenuApiMenuItemsPath', "/menu_items.json");
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService){
+    var narrow = this;
+
+    narrow.searchTerm = "";
+    narrow.getMatchedMenuItems = function()
+      {
+      var promise = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+      promise.then(function (response)
+        {
+        narrow.found = response;
+        }).catch(function (error)
+        {
+        console.log(error.message);
+        });
+      };
+    narrow.removeFromList = function(index)
+      {
+      narrow.found.splice(index,1);
+      }
+  }
+
+  MenuSearchService.$inject = ['$http',
+                               '$filter',
+                               'MenuApiBasePath',
+                               'MenuApiMenuItemsPath',
+                               'searchTermFilter'];
+  function MenuSearchService($http,
+                             $filter,
+                             MenuApiBasePath,
+                             MenuApiMenuItemsPath,
+                             searchTermFilter){
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm)
+      {
+      return $http({method: "GET",
+                    url: (MenuApiBasePath + MenuApiMenuItemsPath)}).
+        then(function (result)
+          {
+          return $filter('searchTerm')(result.data.menu_items, searchTerm);
+          });
+      };
+  }
+
+function FoundItemsDirective()
+  {
+  var ddo = {
+    templateUrl: 'foundItemsList.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'found',
+    bindToController: true
+  };
+  return ddo;
+  }
+
+function FoundItemsDirectiveController()
+  {
+  var foundItems = this;
+  }
+
+
+function SearchTermFilter(){
+  return function(input, searchTerm){
+    var output = [];
+    if (searchTerm)
+      {
+      angular.forEach(input,
+        function (item)
+          {
+          if (item.description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1)
+            {
+            output.push(item)
+            }
+          }
+        );
+      }
+    else
+      {
+      output = input;
+      }
+  return output;
+  };
+}
+})();
